d53faf498358da53801cb5839a0e1d00076897e9
Dasha670 <Mamai.dari@yandex.ru>
2024-10-10 17:27:57 +0300

    Update script_file.js

--- zad5/script_file.js
+++ zad5/script_file.js
@@ -7,7 +7,7 @@
     let p = document.getElementById("price");
     if (f1[0].value.match(/^\d+$/) && f1[0].value>=0){
       r.innerHTML = "Итоговая сумма: " + f1[0].value* f2[0].value;
-      p.innerHTML = "Ценна за кг: "+ f2[0].value;
+      p.innerHTML = "Цена за кг: "+ f2[0].value;
     }
     else {
       alert("Введите целое положительное число без других символов");
