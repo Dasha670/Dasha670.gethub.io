df12df4404e795f8b9369725818a3edd40791588
Dasha670 <Mamai.dari@yandex.ru>
2024-10-12 18:05:41 +0300

    Update script_file.js

--- zad5/script_file.js
+++ zad5/script_file.js
@@ -4,19 +4,18 @@ function click1(event) {
     let f2 = document.getElementsByName("select");
     let r = document.getElementById("resultat");
     let p = document.getElementById("price");
-    if (f1[0].value.match(/^\d+$/) && f1[0].value>=0){
-      r.innerHTML = "Итоговая сумма: " + f1[0].value* f2[0].value;
-      p.innerHTML = "Цена за кг: "+ f2[0].value;
+    if (f1[0].value.match(/^\d+$/) && f1[0].value >= 0) {
+        r.innerHTML = "Итоговая сумма: " + f1[0].value * f2[0].value;
+        p.innerHTML = "Цена за кг: " + f2[0].value;
+    } else {
+        alert("Введите целое положительное число без других символов");
+        r.innerHTML = "";
+        p.innerHTML = "";
     }
-    else {
-      alert("Введите целое положительное число без других символов");
-      r.innerHTML = "";
-      p.innerHTML="";
-    }
-  }
-  window.addEventListener("DOMContentLoaded", function () {
+}
+window.addEventListener("DOMContentLoaded", function () {
     console.log("DOM is Ready");
     let b = document.getElementById("pr");
     b.addEventListener("click", click1);
-  });
+});
 
