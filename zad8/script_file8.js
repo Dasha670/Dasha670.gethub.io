c7f32ed58673d260554de1a8a0681046a6e05cc8
Dasha670 <Mamai.dari@yandex.ru>
2024-11-18 16:43:34 +0300

    Update script_file8.js

--- zad8/script_file8.js
+++ zad8/script_file8.js
@@ -1,30 +1,30 @@
-const openFormBtn = document.getElementById('open-form-button');
-const p = document.getElementById('p');
-const closeBtn = document.querySelector('.close-button');
-const form = document.getElementById('feedback-form');
-const resultDiv = document.getElementById('feedback-result');
+const openFormBtn = document.getElementById("open-form-button");
+const p = document.getElementById("p");
+const closeBtn = document.querySelector(".close-button");
+const form = document.getElementById("feedback-form");
+const resultDiv = document.getElementById("feedback-result");
 
-const localStorageKey = 'feedbackFormValues';
+const localStorageKey = "feedbackFormValues";
 
 function openp() {
-    p.style.display = 'flex';
-    window.history.pushState({ p: true }, '', '#feedback-form');
+    p.style.display = "flex";
+    window.history.pushState({ p: true }, "", "#feedback-form");
     loadFormData();
 }
 
 function closep() {
-    p.style.display = 'none';
-    resultDiv.textContent = '';
+    p.style.display = "none";
+    resultDiv.textContent = "";
     window.history.back();
 }
 
 function saveFormData() {
     const formData = {
-        name: form.name.value,
         email: form.email.value,
-        phone: form.phone.value,
-        organization: form.organization.value,
         message: form.message.value,
+        name: form.name.value,
+        organization: form.organization.value,
+        phone: form.phone.value
     };
     localStorage.setItem(localStorageKey, JSON.stringify(formData));
 }
@@ -32,12 +32,12 @@ function saveFormData() {
 function loadFormData() {
     const savedData = localStorage.getItem(localStorageKey);
     if (savedData) {
-        const { name, email, phone, organization, message } = JSON.parse(savedData);
-        form.name.value = name || '';
-        form.email.value = email || '';
-        form.phone.value = phone || '';
-        form.organization.value = organization || '';
-        form.message.value = message || '';
+        const {email,message,name,organization,phone} = JSON.parse(savedData);
+        form.name.value = name || "";
+        form.email.value = email || "";
+        form.phone.value = phone || "";
+        form.organization.value = organization || "";
+        form.message.value = message || "";
     }
 }
 
@@ -45,33 +45,32 @@ function clearFormData() {
     localStorage.removeItem(localStorageKey);
 }
 
-form.addEventListener('submit', async (e) => {
+form.addEventListener("submit", async (e) => {
     e.preventDefault();
-    resultDiv.textContent = 'Отправка формы...';
+    resultDiv.textContent = "Отправка формы...";
     const formData = new FormData(form);
 
     try {
-        const response = await fetch('https://formcarry.com/s/I6uGHP7kD0s', {
-            method: 'POST',
+        const response = await fetch("https://formcarry.com/s/I6uGHP7kD0s", {
             body: formData,
+            method: "POST"
         });
 
-            resultDiv.textContent = 'Форма успешно отправлена =)';
+            resultDiv.textContent = "Форма успешно отправлена =)";
             clearFormData();
             form.reset();
-        
     } catch (error) {
-        resultDiv.textContent = 'Форма не отправилась =( Попробуйте позже';
+        resultDiv.textContent = "Форма не отправилась =( Попробуйте позже";
     }
 });
 
-openFormBtn.addEventListener('click', openp);
-closeBtn.addEventListener('click', closep);
+openFormBtn.addEventListener("click", openp);
+closeBtn.addEventListener("click", closep);
 
-window.addEventListener('popstate', (event) => {
+window.addEventListener("popstate", (event) => {
     if (!event.state?.p) {
         closep();
     }
 });
 
-form.addEventListener('input', saveFormData)
+form.addEventListener("input", saveFormData);
